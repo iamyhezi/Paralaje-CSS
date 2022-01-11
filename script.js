bd2a01ee06703e5ed503a407df97915350d5d215
Imac <yhesling@hotmail.com>
2022-01-11 06:28:12 -0500

    Paralaje CSS

---
+++ script.js
@@ -0,0 +1,13 @@
+gsap.from(".parallax__title", { opacity: 0, duration: 1, delay: .3, y: 40, easy: "expo.out" });
+
+gsap.from(".parallax__text", { duration: .7, delay: .4, y: 250, easy: "expo.out" });
+
+// gsap.from(".img__bg", { duration: .8, delay: .3, y: 100, easy: "expo.out" });
+// gsap.from(".img__1", { duration: .8, delay: .4, y: 100, easy: "expo.out" });
+// gsap.from(".img__2", { duration: .8, delay: .5, y: 100, easy: "expo.out" });
+// gsap.from(".img__3", { duration: .8, delay: .6, y: 100, easy: "expo.out" });
+// gsap.from(".img__4", { duration: .8, delay: .7, y: 100, easy: "expo.out" });
+// gsap.from(".img__5", { duration: .8, delay: .8, y: 100, easy: "expo.out" });
+// gsap.from(".img__6", { duration: .8, delay: .9, y: 100, easy: "expo.out" });
+// gsap.from(".img__7", { duration: .8, delay: 1, y: 100, easy: "expo.out" });
+// gsap.from(".img__8", { duration: .8, delay: 1.1, y: 100, easy: "expo.out" });
